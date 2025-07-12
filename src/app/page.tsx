bab379ac2a79e3ddd6fc9dd91da4aef8e113c8fa
study forme <studyforme65@gmail.com>
2025-07-12 14:09:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
                 </div>
               </div>
               <Image
-                src="/login-background.jpg"
+                src="/dbeb6909-b451-42f7-ab29-c91b62e6134d.png"
                 width="600"
                 height="400"
                 alt="Hero"

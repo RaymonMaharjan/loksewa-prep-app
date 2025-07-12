f81b8878a01856e38f09ece61a6e66f4cc990ddf
study forme <studyforme65@gmail.com>
2025-07-12 13:47:16 +0000

    .png /public/icons/loksewa_prep_logo_512x512.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         >
             <div className="absolute inset-0 bg-background/60 z-10" />
             <div className="relative z-20 flex flex-col items-center justify-center text-center">
-                <Image src="/icons/logo.svg" alt="Loksewa Prep Logo" width={80} height={80} className="mb-4" />
+                <Image src="/icons/loksewa_prep_logo_512x512.png" alt="Loksewa Prep Logo" width={80} height={80} className="mb-4" />
                 <Loader2 className="h-10 w-10 animate-spin text-primary" />
                 <p className="mt-4 text-lg text-foreground">Loading your experience...</p>
             </div>
